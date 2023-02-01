ac3ccc6293ee46f72c8692c468e6f13ae1269cc1
Caryl Wyatt <caryl.e.wyatt@gmail.com>
2023-02-01 16:39:52 -0600

    update styles

--- src/lib/FeedbackForm.svelte
+++ src/lib/FeedbackForm.svelte
@@ -29,27 +29,53 @@
 
 <form on:submit|preventDefault={onSubmit}>
    
-      <label for="name">Name: </label>
-      <sl-input name="name" id="name" type="name" ></sl-input>
+      <sl-input class="label-on-left" label="Name" name="name" id="name" type="name" ></sl-input>
     
+      <sl-input class="label-on-left" label="Email address" name="email" id="email" type="email" required></sl-input>
    
-      <label for="email">Email address &ast;</label>
-      <sl-input name="email" id="email" type="email" ></sl-input>
+      <sl-input class="label-on-left" label="Short summary" name="summary" id="summary" type="text" required></sl-input>
    
-      <label for="summary">Short summary &ast;</label>
-      <sl-input name="summary" id="summary" type="text" ></sl-input>
-   
-    
-      <label for="bookDescription">Description or URL of the book</label>
-      <sl-input name="bookDescription" id="bookDescription" type="text" ></sl-input>
+      <sl-input class="label-on-left" label="Description or URL of the book" name="bookDescription" id="bookDescription" type="text" ></sl-input>
      
-      <label for="description">Full description or question</label>
-      <sl-textarea name="description" id="description" ></sl-textarea>
+      <sl-textarea class="label-on-left" label="Full description of problem or question" name="description" id="description" ></sl-textarea>
    
       <input name="userURL" id="userURL" type="hidden" bind:value="{userURL}" />
       <input name="userAgent" id="userAgent" type="hidden" bind:value="{userAgent}" />
     
-    
-      <sl-button class="btn" type="submit" value="Submit" aria-label="Submit">Submit</sl-button>
+      <div class="form-options">
+      <sl-button class="btn form-button" variant="default" type="submit" value="Submit" aria-label="Submit">Cancel</sl-button>
+      <sl-button class="btn form-button" variant="primary" type="submit" value="Submit" aria-label="Submit">Submit</sl-button>
+      </div>
    
   </form>
+
+  <style>
+    
+  .label-on-left {
+      --label-width: 150px;
+      --gap-width: 1rem;
+    }
+
+    .label-on-left + .label-on-left {
+      margin-top: var(--sl-spacing-medium);
+    }
+
+    .label-on-left::part(form-control) {
+      display: grid;
+      grid: auto / var(--label-width) 1fr;
+      gap: var(--sl-spacing-3x-small) var(--gap-width);
+      align-items: center;
+    }
+
+    .label-on-left::part(form-control-label) {
+      text-align: right;
+    }
+
+    .form-options {
+      display: flex;
+      justify-content: flex-end;
+      gap:10px;
+      padding: 1em 0;
+    }
+  
+  </style>
