51d9d9cb4ded21d001d4e769d6eb600308496512
Caryl Wyatt <caryl.e.wyatt@gmail.com>
2023-02-01 16:03:01 -0600

    moved config stuff around

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  // probably safe to delete this file now that I moved all the "app" functionality to main.ts
   import FeedbackForm from './lib/FeedbackForm.svelte'
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 // import './app.css'
-import App from "./App.svelte";
+// import App from "./App.svelte";
 
-const app = new App({
-  target: document.getElementById("app"),
+// const app = new App({
+//   target: document.getElementById("app"),
+// });
+
+// export default app;
+
+import FeedbackForm from "./lib/FeedbackForm.svelte";
+
+const apps = [];
+
+document.querySelectorAll("feedback-form").forEach((el) => {
+  const app = new FeedbackForm({
+    target: el,
+  });
+  apps.push(app);
 });
-
-export default app;
