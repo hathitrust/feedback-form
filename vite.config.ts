6eeb76717dc0ff7147cd2f7b0dac5c40a50f976a
Caryl Wyatt <caryl.e.wyatt@gmail.com>
2023-02-14 15:42:26 -0600

    remove vite babel server settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    proxy: {
-      "^/cgi/ping": {
-        target: "https://babel.hathitrust.org",
-        changeOrigin: true,
-      },
-    },
-  },
 });
